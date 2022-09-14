65e6f27ea70d9461561e58493f69dfe8b684bdd8
Rebecca Howard <howardrebecca@gmail.com>
2022-09-14 13:37:15 -0700

    Add models for hake

--- code/model_development.R
+++ code/model_development.R
@@ -14,5 +14,23 @@ yoy_hake <- readRDS(here('data', 'yoy_hake.rdata'))
 # Functions
 
 # Hake ----
-
+# Use models selected during model exploration
+hake_total <- gam(catch + 1 ~ factor(year) + s(longitude, latitude) + s(bottom_depth, k = 4) +
+                    s(julian) + s(temperature, k = 4) + s(salinity, k = 4) + s(longitude, latitude, by = NPGO_pos),
+                  family = tw(link = "log"),
+                  method = 'REML',
+                  data = yoy_hake)
+hake_lower <- gam(lower_cpue + 1 ~ factor(year) + s(longitude, latitude) + s(bottom_depth, k = 4) + 
+                    s(julian) + s(temperature, k = 4) + s(salinity, k = 4) + s(longitude, latitude, by = NPGO_pos),
+                  family = tw(link = "log"),
+                  method = 'REML',
+                  data = yoy_hake)
+hake_upper <- gam(upper_cpue + 1 ~ factor(year) + s(longitude, latitude) + s(bottom_depth, k = 4) + 
+                    s(julian) + s(temperature, k = 4) + s(salinity, k = 4) + s(longitude, latitude, by = NPGO_pos),
+                  family = tw(link = "log"),
+                  method = 'REML',
+                  data = yoy_hake)
+summary(hake_total)
+summary(hake_lower)
+summary(hake_upper)
 
