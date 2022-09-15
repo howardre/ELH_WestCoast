1d8ad0f421e91d33718e58ebff5f09f71ee1c739
Rebecca Howard <howardrebecca@gmail.com>
2022-09-14 17:03:39 -0700

    Test predictions, update formula

--- code/model_development.R
+++ code/model_development.R
@@ -11,13 +11,15 @@ library(tidyr)
 library(purrr)
 
 # Load data
-yoy_hake <- readRDS(here('data', 'yoy_hake.rdata')) %>% drop_na(temperature, salinity, bottom_depth)
+yoy_hake <- readRDS(here('data', 'yoy_hake.rdata')) %>% 
+  drop_na(temperature, salinity, bottom_depth) %>%
+  filter(catch < 711)
 
 # Functions
 
 # Hake ----
 # Leave one group out cross validation
-hake_formula <- formula(catch + 1 ~ s(longitude, latitude) + s(bottom_depth, k = 4) +
+hake_formula <- formula(catch + 1 ~ s(year, bs = "re") + s(longitude, latitude) + s(bottom_depth, k = 4) +
                        s(julian) + s(temperature, k = 4) + s(salinity, k = 4) + s(longitude, latitude, by = NPGO_pos))
 
 # Run GAMs with each year left out
@@ -38,18 +40,21 @@ hake_cv <- lapply(hake_gams, function(x) {
   
   
   
-# Test prediction method
-wo2001_gam <- gam(hake_formula,
+# Test prediction method (getting very large predictions that don't make sense)
+yoy_hake <- dplyr::filter(yoy_hake, catch < 711) # seems like the issue is with outliers?
+wo2000_gam <- gam(hake_formula,
                   family = tw(link = "log"),
                   method = "REML",
-                  data = yoy_hake[yoy_hake$year != 2001, ])
+                  data = yoy_hake[yoy_hake$year != 2000, ])
 
-data_2001 <- dplyr::filter(yoy_hake, year == 2001)
+data_2000 <- dplyr::filter(yoy_hake, year == 2000)
 
-data_2001$pred <- predict(wo2001_gam,
-                          newdata = data_2001,
+data_2000$pred <- predict(wo2000_gam,
+                          newdata = data_2000,
                           type = "response",
                           exclude = "s(year)")
+
+sqrt(mean(data_2000$catch - data_2000$pred, na.rm = T)^2) 
 
 # Use models selected during model exploration
 hake_total <- gam(hake_formula,
