02907d71370b981fcc7491dd2159bf02c6b27a4f
Rebecca Howard <howardrebecca@gmail.com>
2022-09-16 14:44:55 -0700

    Formatting, add code for combining predictions

--- code/model_development.R
+++ code/model_development.R
@@ -108,8 +108,6 @@ hake_small <- gam(hake_small_formula,
 summary(hake_total)
 
 # Leave one group out cross validation
-# Run GAMs with each year left out
-# Leave out one year, run model on remaining data
 hake_small_gams <- lapply(unique(yoy_hake$year), function(x) {
   output <- gam(hake_small_formula,
                 family = tw(link = "log"),
@@ -122,16 +120,16 @@ hake_small_gams <- lapply(unique(yoy_hake$year), function(x) {
 hake_small_results <- hake_data
 for(i in seq_along(hake_small_gams)){
   for(j in seq_along(hake_data)){
-    hake_small_results[[j]]$pred <- predict(hake_small_gams[[i]],
-                                      newdata = hake_data[[j]],
-                                      type = "response",                                   
-                                      exclude = "s(year)")
+    hake_small_results[[j]]$pred_small <- predict(hake_small_gams[[i]],
+                                                  newdata = hake_data[[j]],
+                                                  type = "response",
+                                                  exclude = "s(year)")
   }}
 
 # Calculate RMSE
 # Get values for each year and overall value
 hake_small_RMSE <- lapply(hake_small_results, function(x) {
-  rmse(x$catch, x$pred)
+  rmse(x$lower_cpue, x$pred_small)
 })
 
 mean(unlist(hake_small_RMSE))
@@ -145,7 +143,7 @@ colnames(hake_small_error)[1] <- "RMSE"
 ggplot(hake_small_error) +
   geom_line(aes(year, RMSE),
             size = 1,
-            group = 1) 
+            group = 1) # something weird going on in 2004
 
 # Large
 hake_large_formula <- formula(upper_cpue + 1 ~ s(year, bs = "re") + s(longitude, latitude) + s(bottom_depth, k = 4) +
@@ -159,8 +157,6 @@ hake_large <- gam(hake_large_formula,
 summary(hake_total)
 
 # Leave one group out cross validation
-# Run GAMs with each year left out
-# Leave out one year, run model on remaining data
 hake_large_gams <- lapply(unique(yoy_hake$year), function(x) {
   output <- gam(hake_large_formula,
                 family = tw(link = "log"),
@@ -173,16 +169,16 @@ hake_large_gams <- lapply(unique(yoy_hake$year), function(x) {
 hake_large_results <- hake_data
 for(i in seq_along(hake_large_gams)){
   for(j in seq_along(hake_data)){
-    hake_large_results[[j]]$pred <- predict(hake_large_gams[[i]],
-                                            newdata = hake_data[[j]],
-                                            type = "response",                                   
-                                            exclude = "s(year)")
+    hake_large_results[[j]]$pred_large <- predict(hake_large_gams[[i]],
+                                                  newdata = hake_data[[j]],
+                                                  type = "response",
+                                                  exclude = "s(year)")
   }}
 
 # Calculate RMSE
 # Get values for each year and overall value
 hake_large_RMSE <- lapply(hake_large_results, function(x) {
-  rmse(x$catch, x$pred)
+  rmse(x$upper_cpue, x$pred_large)
 })
 
 mean(unlist(hake_large_RMSE))
@@ -199,3 +195,27 @@ ggplot(hake_large_error) +
             group = 1) 
 
 # Add the predictions for small and large together
+hake_combined_results <- map2(hake_small_results, 
+                              hake_large_results,
+                              ~ .x %>% bind_cols(.y %>% select(pred_large)))
+
+hake_combined_preds <- lapply(hake_combined_results, function(x) {
+  mutate(x, pred = pred_large + pred_small)    
+})
+
+hake_combined_RMSE <- lapply(hake_combined_preds, function(x) {
+  rmse(x$catch, x$pred)
+})
+
+mean(unlist(hake_combined_RMSE))
+
+hake_combined_error <- as.data.frame(do.call(rbind, hake_combined_RMSE))
+hake_combined_error$year <- rownames(hake_combined_error)
+rownames(hake_combined_error) <- NULL
+colnames(hake_combined_error)[1] <- "RMSE"
+
+# Plot the RMSE for each year
+ggplot(hake_combined_error) +
+  geom_line(aes(year, RMSE),
+            size = 1,
+            group = 1) 
