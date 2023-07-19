4d4dbd8153b36b033ff91ae332097864de07cd8a
Rebecca Howard <howardrebecca@gmail.com>
2023-07-19 13:45:33 -0700

    Add functions

---
+++ code/functions/read_data.R
@@ -0,0 +1,15 @@
+read_data <- function(file){
+  yoy <- readRDS(here('data', file)) %>% 
+    tidyr::drop_na(roms_temperature, roms_salinity, roms_ssh, bottom_depth, year, jday, lat, lon) %>%
+    filter(catch < 2500 &
+             year < 2020 &
+             lat < 42) %>%
+    mutate(catch1 = catch + 1,
+           small_catch1 = small + 1,
+           large_catch1 = large + 1,
+           year_f = as.factor(year),
+           ssh_pos = year_ssh + abs(min(year_ssh)) + 10)
+  yoy <- yoy[!(yoy$small == 0 & yoy$large == 0 & yoy$catch > 0), ]
+  yoy_utm <- add_utm_columns(yoy, c("lon", "lat")) # add UTM coordinates
+  return(yoy_utm)
+}
