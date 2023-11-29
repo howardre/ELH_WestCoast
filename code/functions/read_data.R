6996e349cedf2927c0f5d6e49564f28ff8194410
Rebecca Howard <howardrebecca@gmail.com>
2023-11-29 15:14:27 -0800

    Add scaling

--- code/functions/read_data.R
+++ code/functions/read_data.R
@@ -2,13 +2,19 @@ read_data <- function(file){
   yoy <- readRDS(here('data', file)) %>% 
     tidyr::drop_na(roms_temperature, roms_salinity, roms_ssh, bottom_depth, year, jday, lat, lon) %>%
     filter(catch < 2500 &
-             year < 2020) %>%
+             year < 2020 &
+             roms_salinity > 33 &
+             bottom_depth < 2000) %>%
     mutate(catch1 = catch + 1,
            small_catch1 = small + 1,
            large_catch1 = large + 1,
            year_f = as.factor(year),
            ssh_pos = year_ssh + abs(min(year_ssh)) + 10,
-           ssh_scaled = (year_ssh - mean(year_ssh)) / sd(year_ssh))
+           ssh_annual_scaled = (year_ssh - mean(year_ssh)) / sd(year_ssh),
+           depth_scaled = (bottom_depth - mean(bottom_depth)) / sd(bottom_depth),
+           sss_scaled = (roms_salinity - mean(roms_salinity)) / sd(roms_salinity),
+           sst_scaled = (roms_temperature - mean(roms_temperature)) / sd(roms_temperature),
+           ssh_scaled = (ssh_anom - mean(ssh_anom)) / sd(ssh_anom))
   yoy <- yoy[!(yoy$small == 0 & yoy$large == 0 & yoy$catch > 0), ]
   yoy_utm <- add_utm_columns(yoy, 
                              utm_crs = 32610, # UTM 10
