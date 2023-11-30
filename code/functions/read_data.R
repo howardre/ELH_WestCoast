fd6ee8acfaa2e52704ec2428423cb0ed4fc5f463
Rebecca Howard <howardrebecca@gmail.com>
2023-11-29 16:10:17 -0800

    Change scaling, get rid of filters

--- code/functions/read_data.R
+++ code/functions/read_data.R
@@ -2,19 +2,17 @@ read_data <- function(file){
   yoy <- readRDS(here('data', file)) %>% 
     tidyr::drop_na(roms_temperature, roms_salinity, roms_ssh, bottom_depth, year, jday, lat, lon) %>%
     filter(catch < 2500 &
-             year < 2020 &
-             roms_salinity > 33 &
-             bottom_depth < 2000) %>%
+             year < 2020) %>%
     mutate(catch1 = catch + 1,
            small_catch1 = small + 1,
            large_catch1 = large + 1,
            year_f = as.factor(year),
            ssh_pos = year_ssh + abs(min(year_ssh)) + 10,
            ssh_annual_scaled = (year_ssh - mean(year_ssh)) / sd(year_ssh),
-           depth_scaled = (bottom_depth - mean(bottom_depth)) / sd(bottom_depth),
-           sss_scaled = (roms_salinity - mean(roms_salinity)) / sd(roms_salinity),
-           sst_scaled = (roms_temperature - mean(roms_temperature)) / sd(roms_temperature),
-           ssh_scaled = (ssh_anom - mean(ssh_anom)) / sd(ssh_anom))
+           depth_scaled = scale(bottom_depth)[, 1],
+           sss_scaled = scale(roms_salinity)[, 1],
+           sst_scaled = scale(roms_temperature)[, 1],
+           ssh_scaled = scale(ssh_anom)[, 1])
   yoy <- yoy[!(yoy$small == 0 & yoy$large == 0 & yoy$catch > 0), ]
   yoy_utm <- add_utm_columns(yoy, 
                              utm_crs = 32610, # UTM 10
