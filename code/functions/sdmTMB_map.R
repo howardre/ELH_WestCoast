df7e228ed9852fc16e7c31d3e85f7b0e217babb9
Rebecca Howard <howardrebecca@gmail.com>
2023-12-06 16:07:52 -0800

    Add function to plot SVC term

--- code/functions/sdmTMB_map.R
+++ code/functions/sdmTMB_map.R
@@ -69,3 +69,75 @@ sdmTMB_map <- function(df, preds, title, lat_label){
                                 family = "serif",
                                 line = 1))
 }
+
+sdmTMB_SVC <- function(df, preds, title, lat_label){
+  
+  my_color = colorRampPalette(c(sequential_hcl(15, palette = "Mint")))
+  color_levels = 100
+  max_absolute_value = max(abs(c(min(preds$zeta_s_ssh_annual_scaled, na.rm = T),
+                                 max(preds$zeta_s_ssh_annual_scaled, na.rm = T))))
+  color_sequence = seq(max(preds$zeta_s_ssh_annual_scaled, na.rm = T), 
+                       min(preds$zeta_s_ssh_annual_scaled, na.rm = T),
+                       length.out = color_levels + 1)
+  n_in_class = hist(preds$zeta_s_ssh_annual_scaled, breaks = color_sequence, plot = F)$counts > 0
+  col_to_include = min(which(n_in_class == T)):max(which(n_in_class == T))
+  breaks_to_include = min(which(n_in_class == T)):(max(which(n_in_class == T)) + 1)
+  
+  # Make map
+  image(lond,
+        latd,
+        t(matrix(preds$zeta_s_ssh_annual_scaled,
+                 nrow = length(latd),
+                 ncol = length(lond),
+                 byrow = T)),
+        xlim = c(-126, -116),
+        ylim = range(df$lat, na.rm = TRUE) + c(-.4, .5),
+        axes = FALSE,
+        xlab = "",
+        ylab = "")
+  rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "mintcream")
+  par(new = TRUE)
+  image(lond,
+        latd,
+        t(matrix(preds$zeta_s_ssh_annual_scaled,
+                 nrow = length(latd),
+                 ncol = length(lond),
+                 byrow = T)),
+        col = my_color(n = color_levels)[col_to_include],
+        main = title,
+        ylab = lat_label,
+        xlab = "Longitude",
+        xlim = c(-126, -116),
+        ylim = range(df$lat, na.rm = TRUE) + c(-.4, .5),
+        cex.lab = 3.1,
+        cex.axis = 2.3,
+        cex.main = 3.4)
+  maps::map("state",
+            boundary = FALSE,
+            fill = TRUE,
+            col = "wheat4",
+            add = TRUE)
+  text(x = state_labels$lon, 
+       y = state_labels$lat,
+       state_labels$name, 
+       pos = 1,
+       col = "black",
+       cex = 2.6,
+       family = "serif")
+  image.plot(legend.only = T,
+             col = my_color(n = color_levels)[col_to_include],
+             legend.shrink = 0.2,
+             smallplot = c(.3, .35, .11, .24),
+             legend.cex = 1.5,
+             axis.args = list(cex.axis = 1.8,
+                              family = "serif"),
+             legend.width = 0.8,
+             legend.mar = 6,
+             zlim = c(min(preds$zeta_s_ssh_annual_scaled, na.rm = T), 
+                      max(preds$zeta_s_ssh_annual_scaled, na.rm = T)),
+             legend.args = list("log(CPUE + 1)",
+                                side = 2, 
+                                cex = 2.2,
+                                family = "serif",
+                                line = 1))
+}
