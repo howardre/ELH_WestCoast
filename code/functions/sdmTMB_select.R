6c2d743bbdbc6430cfddc80a6a9049f1243a48a8
Rebecca Howard <howardrebecca@gmail.com>
2024-01-29 16:26:21 -0800

    Function to select best sdmTMB model

--- code/functions/sdmTMB_select.R
+++ code/functions/sdmTMB_select.R
@@ -1,4 +1,4 @@
-sdmTMB_select <- function(df, fish_mesh){
+sdmTMB_select_small <- function(df, fish_mesh){
   sdm_full <- sdmTMB(small ~ 0 +
                        s(bottom_depth, k = 5) +
                        s(roms_temperature, k = 5) +
@@ -39,6 +39,63 @@ sdmTMB_select <- function(df, fish_mesh){
                     control = sdmTMBcontrol(newton_loops = 1,
                                             nlminb_loops = 2))
   sdm_jday <- sdmTMB(small ~ 0 +
+                       s(jday, k = 15),
+                     spatial_varying = ~ 0 + ssh_annual_scaled,
+                     data = df,
+                     mesh = fish_mesh,
+                     spatial = "on",
+                     time = "year",
+                     family = tweedie(link = "log"),
+                     spatiotemporal = "ar1",
+                     control = sdmTMBcontrol(newton_loops = 1,
+                                             nlminb_loops = 2))
+  sdm_list <- list(sdm_full, sdm_sss, sdm_sst, sdm_jday)
+  best_sdm <- sdm_list[[which.min(sapply(1:length(sdm_list),
+                                         function(x) min(sdm_list[[x]]$aic)))]]
+  return_list <- list(sdm_list, best_sdm)
+}
+
+sdmTMB_select_large <- function(df, fish_mesh){
+  sdm_full <- sdmTMB(large ~ 0 +
+                       s(bottom_depth, k = 5) +
+                       s(roms_temperature, k = 5) +
+                       s(roms_salinity, k = 5) +
+                       s(jday, k = 15),
+                     spatial_varying = ~ 0 + ssh_annual_scaled,
+                     data = df,
+                     mesh = fish_mesh,
+                     spatial = "on",
+                     time = "year",
+                     family = tweedie(link = "log"),
+                     spatiotemporal = "ar1",
+                     control = sdmTMBcontrol(newton_loops = 1,
+                                             nlminb_loops = 2))
+  sdm_sss <- sdmTMB(large ~ 0 +
+                      s(bottom_depth, k = 5) +
+                      s(roms_temperature, k = 5) +
+                      s(jday, k = 15),
+                    spatial_varying = ~ 0 + ssh_annual_scaled,
+                    data = df,
+                    mesh = fish_mesh,
+                    spatial = "on",
+                    time = "year",
+                    family = tweedie(link = "log"),
+                    spatiotemporal = "ar1",
+                    control = sdmTMBcontrol(newton_loops = 1,
+                                            nlminb_loops = 2))
+  sdm_sst <- sdmTMB(large ~ 0 +
+                      s(bottom_depth, k = 5) +
+                      s(jday, k = 15),
+                    spatial_varying = ~ 0 + ssh_annual_scaled,
+                    data = df,
+                    mesh = fish_mesh,
+                    spatial = "on",
+                    time = "year",
+                    family = tweedie(link = "log"),
+                    spatiotemporal = "ar1",
+                    control = sdmTMBcontrol(newton_loops = 1,
+                                            nlminb_loops = 2))
+  sdm_jday <- sdmTMB(large ~ 0 +
                        s(jday, k = 15),
                      spatial_varying = ~ 0 + ssh_annual_scaled,
                      data = df,
