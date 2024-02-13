0a97b7c21ff295847e7746d55065a4c45af8c620
Rebecca Howard <howardrebecca@gmail.com>
2024-02-13 14:35:01 -0800

    Fix variable names, add new vars

--- code/functions/read_data.R
+++ code/functions/read_data.R
@@ -1,18 +1,23 @@
 read_data <- function(file){
   yoy <- readRDS(here('data', file)) %>% 
-    tidyr::drop_na(roms_temperature, roms_salinity, roms_ssh, bottom_depth, year, jday, lat, lon) %>%
+    tidyr::drop_na(roms_sst, roms_sss, roms_ssh, bottom_depth, year, jday, lat, lon) %>%
     filter(catch < 2500 &
-             year < 2020) %>%
+             year < 2020 & year > 1994) %>%
     mutate(catch1 = catch + 1,
            small_catch1 = small + 1,
            large_catch1 = large + 1,
            year_f = as.factor(year),
-           ssh_pos = year_ssh + abs(min(year_ssh)) + 10,
-           ssh_annual_scaled = scale((year_ssh - mean(year_ssh)) / sd(year_ssh))[, 1],
            depth_scaled = scale(bottom_depth)[, 1],
-           sss_scaled = scale(roms_salinity)[, 1],
-           sst_scaled = scale(roms_temperature)[, 1],
-           ssh_scaled = scale(ssh_anom)[, 1])
+           sss_scaled = scale(roms_sss)[, 1],
+           sst_scaled = scale(roms_sst)[, 1],
+           ssh_scaled = scale(roms_ssh)[, 1],
+           vgeo = scale(vgeo)[, 1], 
+           v_cu = scale(v_cu)[, 1], 
+           vmax_cu = scale(vmax_cu)[, 1],
+           u_vint_50m = scale(u_vint_50m)[, 1], 
+           u_vint_100m = scale(u_vint_100m)[, 1], 
+           depth_iso26 = scale(depth_iso26)[, 1], 
+           spice_iso26 = scale(spice_iso26)[, 1])
   yoy <- yoy[!(yoy$small == 0 & yoy$large == 0 & yoy$catch > 0), ]
   yoy_utm <- add_utm_columns(yoy, 
                              utm_crs = 32610, # UTM 10
