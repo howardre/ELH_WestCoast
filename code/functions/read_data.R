7f7e046fbd465afd5442112bb7a3eff83f117e3c
Rebecca Howard <howardrebecca@gmail.com>
2024-03-03 10:20:17 -0800

    Change year range

--- code/functions/read_data.R
+++ code/functions/read_data.R
@@ -2,7 +2,7 @@ read_data <- function(file){
   yoy <- readRDS(here('data', file)) %>% 
     tidyr::drop_na(sst, sss, bottom_depth, year, jday, latitude, longitude) %>%
     filter(catch < 2500 &
-             year < 2020 & year > 2010) %>%
+             year < 2020 & year > 1993) %>%
     mutate(catch1 = catch + 1,
            pres_small = ifelse(small > 0, 1, 0),
            pres_large = ifelse(large > 0, 1, 0),
