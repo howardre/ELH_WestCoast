44891ad71b23ce54713a013d01ca5255429e1a0c
Rebecca Howard <howardrebecca@gmail.com>
2024-03-29 15:29:10 -0700

    New intro map code

--- code/data_map.R
+++ code/data_map.R
@@ -1,78 +1,68 @@
 # Libraries
-library(marmap)
+library(ggOceanMaps)
+library(usmap)
 library(here)
 
-# Load fish data
-yoy_hake_catch <- readRDS(here('data', 'yoy_hake.Rdata'))
-yoy_hake_catch$presence <- 1 * (yoy_hake_catch$catch > 0)
-
-# Load bathymetry
-WC_bathy <- getNOAA.bathy(lon1 = -125.7, lon2 = -116.5, 
-                          lat1 = 48.6, lat2 = 32.3, 
-                          resolution = 1)
-blues <- c("lightsteelblue4", "lightsteelblue3", "lightsteelblue2", "lightsteelblue1")
-greys <- c(grey(0.6), grey(0.93), grey(0.99))
+# Create palettes and data frames
+bathy_palette <- colorRampPalette(c("lightsteelblue1", "lightsteelblue4"))(9)
+CA_bathy <- data.frame(lon = c(-126, -116),
+                       lat = c(49, 32))
+usa <- us_map(exclude = c("AK", "HI"))
+usa_sf <- sf::st_as_sf(usa, 
+                       coords = c("lon", "lat"),
+                       crs = sf::st_crs(4326))
 
 # Create labels for the states
-state_labels <- data.frame(name = c("Washington", "Oregon", "California"),
-                           lat = c(47.5, 45.0, 37.0),
-                           lon = c(-120.0, -121.0, -119.5))
+text_labels <- data.frame(name = c("Washington", "Oregon", "California"),
+                          lat = c(47.5, 43.0, 37.0),
+                          lon = c(-120.0, -121.0, -119.5))
+text_sf <- sf::st_as_sf(text_labels, 
+                        coords = c("lon", "lat"),
+                        crs = sf::st_crs(4326))
 
-# Create map
-windows(width = 20,
-        height = 32,)
-par(mfrow = c(1, 1),
-    family = 'serif',
-    mar = c(4, 5, 3, .2) + .15)
-plot.bathy(WC_bathy,
-           image = T,
-           axes = T,
-           lwd = 0.03,
-           land = T,
-           n = 0,
-           asp = NA,
-           bpal = list(c(0,
-                         max(WC_bathy),
-                         greys),
-                       c(min(WC_bathy),
-                         0,
-                         blues)),
-           xlim = c(-125.7, -116.5),
-           ylim = range(yoy_hake_catch$lat, na.rm = TRUE) + c(-.4, .5),
-           ylab = "Latitude °N",
-           xlab = "Longitude °W",
-           main = "",
-           cex.lab = 1.2,
-           cex.main = 1.7,
-           cex.axis = 1)
-plot(WC_bathy,
-     deep = -200,
-     shallow = -200,
-     lwd = 0.4,
-     drawlabels = T,
-     add = T,
-     col = "slategrey")
-plot(WC_bathy,
-     deep = -2000,
-     shallow = -2000,
-     lwd = 0.4,
-     drawlabels = T,
-     add = T,
-     col =  "slategrey")
-maps::map("state",
-          boundary = FALSE,
-          fill = TRUE,
-          col = "wheat4",
-          add = TRUE)
-text(x = state_labels$lon, 
-     y = state_labels$lat,
-     state_labels$name, 
-     pos = 1,
-     col = "black",
-     cex = 1.2,
-     family = "serif")
-points(yoy_hake_catch$lon,
-       yoy_hake_catch$lat,
-       pch = 18,
-       col = 'darkmagenta',
-       cex = .9)
+
+# Make map
+CCE_map <- basemap(data = CA_bathy,
+                   bathymetry = TRUE,
+                   rotate = TRUE,
+                   legends = FALSE,
+                   land.col = "wheat4",
+                   grid.col = NA,
+                   lon.interval = 4,
+                   lat.interval = 3) +
+  geom_polygon(data = transform_coord(CA_bathy),
+               aes(x = lon, y = lat),
+               fill = NA) +
+  scale_fill_manual(values = bathy_palette) +
+  labs(x = "Longitude",
+       y = "Latitude")  +
+  ggspatial::annotation_north_arrow(location = "tr",
+                                    which_north = "true",
+                                    style = ggspatial::north_arrow_nautical(text_family = "serif"),
+                                    height = unit(3, "cm"),
+                                    width = unit(3, "cm")) +
+  # ggspatial::annotation_scale(location = "bl",
+  #                             text_family = "serif",
+  #                             style = "ticks",
+  #                             height = unit(0.7, "cm"),
+  #                             text_cex = 1.6,
+  #                             line_width = 1.8) +
+  geom_sf_text(data = text_sf,
+               aes(label = name),
+               size = 7,
+               family = "serif") +
+  theme(axis.text = element_text(family = "serif", size = 20),
+        axis.title = element_text(family = "serif", size = 23),
+        strip.text = element_text(family = "serif", size = 21))
+
+CCE_map
+
+# Save map
+dev.copy(jpeg,
+         here('manuscripts/Figures',
+              'Figure1.jpg'),
+         height = 18,
+         width = 12,
+         res = 200,
+         units = 'in')
+dev.off()
