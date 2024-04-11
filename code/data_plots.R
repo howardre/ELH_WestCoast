bf4adc1581068c6116892e718306956c139274b2
Rebecca Howard <howardrebecca@gmail.com>
2024-04-11 16:42:21 -0700

    Add faceted maps

---
+++ code/data_plots.R
@@ -0,0 +1,67 @@
+### Title: Raw Data Plots
+### Author: Rebecca Howard
+### Date: 04/11/2024
+
+library(here)
+library(ggplot2)
+library(dplyr)
+
+# Load data
+yoy_hake <- filter(read_data('yoy_hake.Rdata'), year > 2002) 
+yoy_anchovy <- filter(read_data('yoy_anch.Rdata'), latitude < 42, year > 2013)
+yoy_widow <- filter(read_data('yoy_widw.Rdata'), year > 2000)
+yoy_shortbelly <- filter(read_data('yoy_sbly.Rdata'), year > 2000)
+yoy_sdab <- filter(read_data('yoy_dab.Rdata'), year > 2012) # doesn't work otherwise
+yoy_squid <- read_data('yoy_squid.Rdata')
+
+west_coast <- map_data("usa")
+
+# Map function
+make_maps <- function(data, size){
+  ggplot() +  
+    geom_polygon(aes(long, lat, group = group), data = west_coast,
+                 fill = "lightyellow4", 
+                 colour = "black") +
+    geom_point(data = data, 
+               aes(longitude, latitude,
+                   size = size),
+               color = "salmon") +
+    coord_quickmap(xlim = c(-126, -117), ylim = c(32, 48)) +
+    theme_classic() +
+    theme(panel.background = element_rect(fill = "gray91", colour = "gray91"),
+          axis.line = element_blank(),
+          axis.ticks = element_blank(),
+          plot.title = element_text(size = 22, family = "serif", face = "bold"),
+          axis.text = element_text(family = "serif", size = 14),
+          axis.title = element_text(family = "serif", size = 18),
+          axis.text.x = element_text(angle = 45, vjust = 0.7),
+          strip.text = element_text(family = "serif", size = 18),
+          legend.title = element_text(family = "serif", size = 16),
+          legend.text = element_text(family = "serif", size = 14)) +
+    facet_wrap(~ year, nrow = 2) 
+}
+
+# Faceted maps
+# Pacific Hake
+make_maps(yoy_hake, yoy_hake$small)
+make_maps(yoy_hake, yoy_hake$large)
+
+# Shortbelly Rockfish
+make_maps(yoy_shortbelly, yoy_shortbelly$small)
+make_maps(yoy_shortbelly, yoy_shortbelly$large)
+
+# Widow Rockfish
+make_maps(yoy_widow, yoy_widow$small)
+make_maps(yoy_widow, yoy_widow$large)
+
+# Pacific Sanddab
+make_maps(yoy_sdab, yoy_sdab$small)
+make_maps(yoy_sdab, yoy_sdab$large)
+
+# Market Squid
+make_maps(yoy_squid, yoy_squid$small)
+make_maps(yoy_squid, yoy_squid$large)
+
+# Northern Anchovy
+make_maps(yoy_anchovy, yoy_anchovy$small)
+make_maps(yoy_anchovy, yoy_anchovy$large)
